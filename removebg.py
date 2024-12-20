0cb18fd85d2bfdbd645d86e8e1230757b14f3aaf
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-20 17:01:34 +0800

    Initial Commit

---
+++ removebg.py
@@ -0,0 +1,8 @@
+from rembg import remove
+from PIL import Image
+
+input_path = './images/image1.jpg'
+output_path = './outputImage/image1.png'
+inp = Image.open(input_path)
+output = remove(inp)
+output.save(output_path)
