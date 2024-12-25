3640a2aafdfbb821d204ed09eea6b4c2c4ed29cf
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-25 15:43:24 +0800

    Renaming Files

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os;
+
 def separateCharAndNum(str):
     char = []
     num = []
@@ -8,3 +10,10 @@ def separateCharAndNum(str):
             num.append(i)
     return char, num
 print(separateCharAndNum("test123"));
+
+def renameFiles():
+    path = './outputImage/'
+    files = os.listdir(path);
+    for i, file in enumerate(files):
+        os.rename(os.path.join(path, file), os.path.join(path, f'result{i + 1}.png'));
+    print("Files renamed successfully");
