752dc41f543b747ee81a6d02764484ab30dfdf18
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-26 08:09:40 +0800

    Add createPath, removePath

--- test.py
+++ test.py
@@ -17,3 +17,19 @@ def renameFiles():
     for i, file in enumerate(files):
         os.rename(os.path.join(path, file), os.path.join(path, f'result{i + 1}.png'));
     print("Files renamed successfully");
+    
+
+def createPath():
+    path = './outputImage/'
+    if not os.path.exists(path):
+        os.makedirs(path);
+    print("Path created successfully");
+    
+
+def removePath():
+    path = './outputImage/'
+    if os.path.exists(path):
+        os.rmdir(path);
+    print("Path removed successfully");
+
+
